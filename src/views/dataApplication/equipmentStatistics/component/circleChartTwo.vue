dbf2a99e6ad27cb3d69bf69429ddb9f63e659144
gxj <>
2019-09-06 16:05:45 +0800

    feat:(views-equipmentStatistics): 1# 添加新页面;

---
+++ src/views/dataApplication/equipmentStatistics/component/circleChartTwo.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <div :id="chartId" class="chart-style"></div>
+  </div>
+</template>
+
+<script>
+import { debounce } from '@/utils'
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+export default {
+  // 父组件传入的属性值
+  props: {
+    chartId: null,
+    titleName: null,
+    chartDataTwo: null
+  },
+  data() {
+    return {
+      chart: null,
+      chartOptions: {
+        title: {
+          text: this.titleName,
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{d}%'
+        },
+        legend: {
+          type: 'scroll',
+          orient: 'vertical',
+          right: 10,
+          top: 20,
+          bottom: 20,
+          data: ['在线', '离线']
+        },
+        series: [
+          {
+            name: '姓名',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: this.chartDataTwo,
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  computed: {
+    // 是否为手机
+    isMobile() {
+      return this.$store.state.app.isMobile
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.updateResize()
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    window.removeEventListener('resize', this.__resizeHanlder)
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    // 初始化图表
+    initChart() {
+      this.chart = echarts.init(document.getElementById(this.chartId))
+      this.chart.setOption(this.chartOptions)
+    },
+    //  自动适配宽度
+    updateResize() {
+      this.__resizeHanlder = debounce(() => {
+        if (this.chart) {
+          this.chart.resize()
+        }
+      }, 100)
+      window.addEventListener('resize', this.__resizeHanlder)
+    },
+    // 清除图表数据
+    clearChartData() {
+      this.chartOptions.xAxis.data.length = 0
+      for (var i = 0; i < 6; i++) {
+        this.chartOptions.series[i].data.length = 0
+      }
+      // 重新初始化
+      for (i = 0; i < 300; i++) {
+        this.chartOptions.xAxis.data.push(this.$dayjs().subtract(300 - i, 'second').format('HH:mm:ss'))
+        for (var j = 0; j < 6; j++) {
+          this.chartOptions.series[j].data.push(0)
+        }
+      }
+      this.chart.setOption(this.chartOptions)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chart-style {
+  width: 100%;
+  height: 220px;
+}
+</style>
---
+++ src/views/dataApplication/equipmentStatistics/component/failureRateChart.vue
@@ -0,0 +1,118 @@
+<template>
+  <div :id="chartId" style="width:100%;height:220px"></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import { debounce } from '@/utils'
+require('echarts/theme/macarons') // echarts theme
+// const animationDuration = 6000
+
+export default {
+  // 父组件传入的属性值
+  props: {
+    chartId: null,
+    titleName: null
+  },
+  data() {
+    return {
+      yAxis: [],
+      chart: null,
+      options: {
+        title: {
+          text: this.titleName,
+          left: 'center'
+        },
+        color: ['#3398DB'],
+        // tooltip: {
+        //   trigger: 'axis',
+        //   axisPointer: {
+        //     type: 'shadow'
+        //   }
+        // },
+        grid: {
+          left: 40,
+          right: 10,
+          top: 30,
+          bottom: 30
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['2008', '2009', '2010', '2011', '2012', '2013', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
+            axisTick: {
+              alignWithLabel: true
+            },
+            axisLabel: {
+              interval: 0
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '记录显示',
+            type: 'bar',
+            barWidth: '50%',
+            data: [10, 30, 20, 39, 65, 100, 10, 30, 20, 39, 65, 100, 50]
+          }, {
+            type: 'bar',
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+                  offset: 0,
+                  color: '#508DFF'
+                }, {
+                  offset: 1,
+                  color: '#26C5FE'
+                }])
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.doResizeChart()
+  },
+  created() {
+  },
+  beforeDestroy() {
+    this.closeResize()
+  },
+  methods: {
+    // 初始化图表
+    initChart() {
+      this.chart = this.$echarts.init(document.getElementById(this.chartId))
+      this.chart.setOption(this.options)
+    },
+    // 图表自适应处理
+    doResizeChart() {
+      this.__resizeHanlder = debounce(() => {
+        if (this.chart) {
+          this.chart.resize()
+        }
+      }, 100)
+      window.addEventListener('resize', this.__resizeHanlder)
+    },
+    // 关闭图表自适应
+    closeResize() {
+      if (!this.chart) {
+        return
+      }
+      window.removeEventListener('resize', this.__resizeHanlder)
+      this.chart.dispose()
+      this.chart = null
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
+
