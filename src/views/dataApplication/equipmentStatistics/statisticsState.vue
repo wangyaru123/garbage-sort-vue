1e7228e05b07ba065621e30bb0e70cf06882dcc8
xixuyang <123456>
2019-09-11 08:43:15 +0800

    feet(views\dataApplication\salesStatistics&router\modules\dataApplication.js&views\dataApplication\statisticsState.vue)1#：增加销售查询模块，2#：修改新增模块名称（销售查询、设备状态），3#：设备状态页面代码整理。

--- src/views/dataApplication/equipmentStatistics/statisticsState.vue
+++ src/views/dataApplication/equipmentStatistics/statisticsState.vue
@@ -116,25 +116,11 @@
           </el-col>
         </el-card>
       </el-col>
-      <!--按省份统计-->
-      <el-col :span="24">
-        <el-card class="mt-5">
-          <statisticsStateChart chartId="mobilityChart" titleName="按省份统计"></statisticsStateChart>
-        </el-card>
-      </el-col>
-      <!--按年份统计-->
-      <el-col :span="24">
-        <el-card class="mt-5">
-          <statisticsStateChartByYear chartId="outputChart" titleName="按年份统计"></statisticsStateChartByYear>
-        </el-card>
-      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import statisticsStateChart from './component/statisticsStateChart'
-import statisticsStateChartByYear from './component/statisticsStateChartByYear'
 import utilizationRateChart from './component/utilizationRateChart'
 import CircleChart from './component/circleChart'
 import failureRateChart from './component/failureRateChart'
@@ -143,8 +129,6 @@ import { getRateByYear, getRateByMouth, getRateByDay, getRateByTime } from '@/ap
 
 export default {
   components: {
-    statisticsStateChart,
-    statisticsStateChartByYear,
     utilizationRateChart,
     CircleChart,
     failureRateChart,
---
+++ src/views/dataApplication/salesStatistics/statisticsState.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="mt-5">
+    <el-row :gutter="5">
+      <!--按省份统计-->
+      <el-col :span="24">
+        <el-card class="mt-5">
+          <statisticsStateChart chartId="mobilityChart" titleName="按省份统计"></statisticsStateChart>
+        </el-card>
+      </el-col>
+      <!--按年份统计-->
+      <el-col :span="24">
+        <el-card class="mt-5">
+          <statisticsStateChartByYear chartId="outputChart" titleName="按年份统计"></statisticsStateChartByYear>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import statisticsStateChart from './component/statisticsStateChart'
+import statisticsStateChartByYear from './component/statisticsStateChartByYear'
+
+export default {
+  components: {
+    statisticsStateChart,
+    statisticsStateChartByYear
+  },
+  data: function () {
+    return {
+    }
+  },
+  created() {
+  },
+  mounted: function () {
+  },
+  beforeDestroy() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
