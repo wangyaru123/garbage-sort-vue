12df8ae8b02e8dfb3fe1ddd3aad1581580f8036f
Shuang <xingshuang_cool@163.com>
2019-09-17 15:40:47 +0800

    feat(views-billboard-detail): 设备详情信息看板页面的初步实现，还有很多待完善;

---
+++ src/views/billboard/detail/component/deviceState.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="main-style">
+    <div class="corner top-left-corner"></div>
+    <div class="corner top-right-corner"></div>
+    <div class="corner bottom-left-corner"></div>
+    <div class="corner bottom-right-corner"></div>
+    <div class="content">
+      <div class="title">设备状态</div>
+      <div class="data">
+        <el-row v-for="index in 6" :key="index" class="data-style">
+          <el-col :span="10">浙江**大学{{index}}</el-col>
+          <el-col :span="7" class="text-c">
+            <el-tag size="mini" effect="dark" type="success">在线 </el-tag>
+            <span>&nbsp; 5</span>
+          </el-col>
+          <el-col :span="7" class="text-c">
+            <el-tag size="mini" effect="dark" type="danger">离线 </el-tag>
+            <span>&nbsp; 5</span>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+  },
+  computed: {
+    isMobile() {
+      return this.$store.state.app.isMobile
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-style {
+  background-color: #001e31;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  box-shadow: inset 0px 0px 4px 0px #00ffff;
+}
+.corner {
+  position: absolute;
+  width: 25px;
+  height: 25px;
+}
+.top-left-corner {
+  top: -1px;
+  left: -1px;
+  border-left: 3px solid #009fff;
+  border-top: 3px solid #009fff;
+}
+.top-right-corner {
+  top: -1px;
+  right: -1px;
+  border-right: 3px solid #009fff;
+  border-top: 3px solid #009fff;
+}
+.bottom-left-corner {
+  bottom: -1px;
+  left: -1px;
+  border-left: 3px solid #009fff;
+  border-bottom: 3px solid #009fff;
+}
+.bottom-right-corner {
+  bottom: -1px;
+  right: -1px;
+  border-right: 3px solid #009fff;
+  border-bottom: 3px solid #009fff;
+}
+.content {
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  .title {
+    font-size: 20px;
+    height: 30px;
+    padding-left: 10px;
+    color: #dee3e6;
+    background: -webkit-linear-gradient(left, #00d1fa, #064975, #001e31);
+  }
+}
+.data {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: calc(100% - 30px);
+  width: 100%;
+}
+.data-style {
+  border-bottom: 1px solid #124667;
+  height: 35px;
+  margin: 0px 15px;
+  padding-top: 5px;
+}
+</style>
--- src/views/billboard/detail/index.vue
+++ src/views/billboard/detail/index.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>详情</div>
+  <div class="total-layout">
+    <div class="title">“1+X”工业机器人应用编程</div>
+    <div class="content">
+      <el-col :lg="5" :md="5" :sm="5" class="content-layout">
+        <div class="item-h33 p-10">
+          <device-info></device-info>
+        </div>
+        <div class="item-h33 p-10">
+          <device-state></device-state>
+        </div>
+        <div class="item-h33 p-10">
+          <parameter></parameter>
+        </div>
+      </el-col>
+      <el-col :lg="14" :md="14" :sm="14" class="content-layout">
+        <div class="item-h50">
+          <el-col :lg="12" :md="12" :sm="12" class="h100 p-10">
+            <operation-ratio></operation-ratio>
+          </el-col>
+          <el-col :lg="12" :md="12" :sm="12" class="h100 p-10">
+            <use-duration></use-duration>
+          </el-col>
+        </div>
+        <div class="item-h50 p-10">
+          <robot-info></robot-info>
+        </div>
+      </el-col>
+      <el-col :lg="5" :md="5" :sm="5" class="content-layout">
+        <div class="item-h33 p-10">
+          <device-location></device-location>
+        </div>
+        <div class="item-h33 p-10">
+          <device-alarm></device-alarm>
+        </div>
+        <div class="item-h33 p-10">
+          <box-info></box-info>
+        </div>
+      </el-col>
+    </div>
+  </div>
 </template>
 
 <script>
+import boxInfo from './component/boxInfo'
+import deviceAlarm from './component/deviceAlarm'
+import deviceInfo from './component/deviceInfo'
+import deviceLocation from './component/deviceLocation'
+import deviceState from './component/deviceState'
+import operationRatio from './component/operationRatio'
+import parameter from './component/parameter'
+import robotInfo from './component/robotInfo'
+import useDuration from './component/useDuration'
+
 export default {
+  components: {
+    boxInfo,
+    deviceAlarm,
+    deviceInfo,
+    deviceLocation,
+    deviceState,
+    operationRatio,
+    parameter,
+    robotInfo,
+    useDuration
+  },
   data() {
     return {
 
@@ -13,4 +73,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.total-layout {
+  background-color: #002035;
+  height: 100vh;
+  width: 100vw;
+  color: white;
+}
+.title {
+  height: 80px;
+  width: 100%;
+  text-align: center;
+  line-height: 80px;
+  font-size: 28px;
+  color: #00ffcc;
+  border-bottom: 1px solid #009fff;
+}
+.content {
+  height: calc(100vh - 80px);
+  color: white;
+  .content-layout {
+    height: 100%;
+  }
+}
+.item-h33 {
+  height: 33.3%;
+}
+.item-h50 {
+  height: 50%;
+}
+.h100 {
+  height: 100%;
+}
 </style>
